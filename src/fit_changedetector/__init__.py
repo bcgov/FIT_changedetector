fcb0bc7a91844ae0adc03248872a60fabe5d27f4
Simon Norris <snorris@hillcrestgeo.ca>
2024-09-23 09:13:19 -0700

    reorganize for easier testing on gts

--- src/fit_changedetector/__init__.py
+++
@@ -1,3 +0,0 @@
-from .diff import gdf_diff, add_synthetic_primary_key
-
-__version__ = "0.0.1a1"
--- test_diffs.py
+++ test_diffs.py
@@ -1,13 +1,13 @@
 import geopandas
-import fit_changedetector as fcd
+from changedetector import gdf_diff
 
 
 def test_diff():
     # one instance of each type of change is found
     # when comparing these two test files
-    df_a = geopandas.read_file("tests/data/test_parks_a.geojson")
-    df_b = geopandas.read_file("tests/data/test_parks_b.geojson")
-    d = fcd.gdf_diff(df_a, df_b, primary_key="fcd_load_id", return_type="gdf")
+    df_a = geopandas.read_file("test_parks_a.geojson")
+    df_b = geopandas.read_file("test_parks_b.geojson")
+    d = gdf_diff(df_a, df_b, primary_key="fcd_load_id", return_type="gdf")
     assert len(d["NEW"] == 1)
     assert len(d["DELETED"] == 1)
     assert len(d["MODIFIED_BOTH"] == 1)
