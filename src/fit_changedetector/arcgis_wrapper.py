15cc57a37d38d261e2a5589b872e4832dd8c6558
Simon Norris <snorris@hillcrestgeo.ca>
2024-10-22 16:56:33 -0700

    update arc wrapper to latest bare minimum

--- src/fit_changedetector/arcgis_wrapper.py
+++ src/fit_changedetector/arcgis_wrapper.py
@@ -1,11 +1,12 @@
 import subprocess
+from pathlib import Path
 
 import arcpy
 
 ENV = "Q:\\dss_workarea\\_contractors\\sinorris\\FIT_changedetector\\fcd_env"
 
-dataset_a = arcpy.GetParameterAsText(0)
-dataset_b = arcpy.GetParameterAsText(1)
+data_original = arcpy.GetParameterAsText(0)
+data_new = arcpy.GetParameterAsText(1)
 primary_key = arcpy.GetParameter(2)
 
 # -- todo --
@@ -15,7 +16,13 @@ primary_key = arcpy.GetParameter(2)
 # precision = int(arcpy.GetParameterAsText(5))
 # debug = arcpy.GetParameter(6)
 
+# parse data source paths - gdb only
+gdb_original = Path(data_original).parent
+layer_original = Path(data_original).name
+gdb_new = Path(data_new).parent
+layer_new = Path(data_new).name
+
 subprocess.run(
-    f"CALL conda.bat deactivate && conda activate {ENV} && changedetector {dataset_a} {dataset_b} -k {primary_key}",
+    f"CALL conda.bat deactivate && conda activate {ENV} && changedetector compare {gdb_original} {gdb_new} --layer-a {layer_original} --layer-b {layer_new} -pk {primary_key}",
     shell=True,
 )
